9df46afacbcca9158275e5e8e3fec1a1c6287a44
Khalil Charfi <khalilcharfi@users.noreply.github.com>
2025-03-12 21:52:49 +0100

    Add files via upload

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: 'js/app.js',
+  output: {
+    dir: 'dist',
+    format: 'es'
+  },
+  plugins: [
+    nodeResolve(),
+    copy({
+      targets: [
+        { src: 'index.html', dest: 'dist' },
+        { src: 'style.css', dest: 'dist' },
+        { src: 'data.json', dest: 'dist' }
+      ]
+    })
+  ]
+};
